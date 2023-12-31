7a457fe4a04800d54b0867fdee56c79a3bd7b17f
antan <154360385+antan1011@users.noreply.github.com>
2023-12-31 20:12:20 +0800

    Update BeanConfig.java

--- BeanConfig.java
+++ BeanConfig.java
@@ -1,6 +1,5 @@
 package com.example.system.config;
 
-import com.example.system.utils.SnowflakeIdGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,27 +7,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 @Configuration
 public class BeanConfig {
-    @Bean("imageName")
-    public SnowflakeIdGenerator imageName() {
-        return new SnowflakeIdGenerator(5);
-    }
-    @Bean("articleId")
-    public SnowflakeIdGenerator articleId() {
-        return new SnowflakeIdGenerator(4);
-    }
-    @Bean("recordId")
-    public SnowflakeIdGenerator recordId() {
-        return new SnowflakeIdGenerator(3);
-    }
-    @Bean
-    public ExecutorService executorService() {
-        return Executors.newSingleThreadExecutor();
-    }
     @Bean
     public RedisTemplate<String,?> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, ?> template = new RedisTemplate<>();
